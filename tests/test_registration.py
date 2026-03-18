8b18b61b4bc5f9d3acc6aea62a561ab08478586c
Haalu-Kai <tinavie00@gmail.com>
2026-03-18 10:32:52 +0300

    Update test_registration.py

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,8 +1,6 @@
 import pytest
-import time
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 from web_locators.locators import *
 from data.urls import Urls
 from data.data import ValidData
@@ -17,55 +15,63 @@ class TestStellarBurgersRegistration:
         driver.find_element(*AuthRegistre.ar_name_field).send_keys(ValidData.user_name)
         driver.find_element(*AuthRegistre.ar_email_field).send_keys(ValidData.login)
         driver.find_element(*AuthRegistre.ar_password_field).send_keys(ValidData.password)
-
         driver.find_element(*AuthRegistre.ar_register_button).click()
-        WebDriverWait(driver, 8).until(EC.presence_of_element_located(AuthLogin.al_element_with_login_text))
 
-        login_button = driver.find_element(*AuthLogin.al_element_with_login_text)
-        assert driver.current_url == Urls.url_login and login_button.text == 'Вход'
+        login_button = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located(AuthLogin.al_button_with_text_vhod)
+        )
+
+        assert driver.current_url == Urls.url_login, "После регистрации не перешли на страницу логина"
+        assert login_button.is_displayed(), "Кнопка 'Вход' не отображается на странице логина"
 
     def test_registration_empty_name_nothing_happens(self, driver):
-        """ При пустом поле Имя ничего не происходит: ошибки и перехода на страницу входа нет """
+        """При пустом поле Имя ничего не происходит: остаёмся на странице, ошибки нет"""
         driver.get(Urls.url_register)
 
         driver.find_element(*AuthRegistre.ar_email_field).send_keys('test1@yan.ru')
         driver.find_element(*AuthRegistre.ar_password_field).send_keys('124567')
-
         driver.find_element(*AuthRegistre.ar_register_button).click()
-        WebDriverWait(driver, 5).until(EC.element_to_be_clickable(AuthRegistre.ar_register_button))
-        time.sleep(2)
-        errors_messages = driver.find_elements(*AuthRegistre.ar_error_message)
 
-        assert driver.current_url == Urls.url_register and len(errors_messages) == 0
+        WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable(AuthRegistre.ar_register_button)
+        )
 
+        error_messages = driver.find_elements(*AuthRegistre.ar_error_message)
 
-    @pytest.mark.parametrize('email_list', ['test1@yanru', 'test2yan.ru', 'te st3@yan.ru', 'test4@ya n.ru',
-                                            '@yan.ru', 'test6@.ru', 'test7@yan.'])
-    def test_registration_incorrect_email_show_error(self, driver, email_list):
-        """ При некорректном email появляется ошибка, что пользователь уже существует """
+        assert driver.current_url == Urls.url_register, "Произошёл неожиданный переход со страницы регистрации"
+        assert len(error_messages) == 0, "Появилась ошибка при пустом имени"
+
+    @pytest.mark.parametrize('invalid_email', [
+        'test1@yanru', 'test2yan.ru', 'te st3@yan.ru', 'test4@ya n.ru',
+        '@yan.ru', 'test6@.ru', 'test7@yan.'
+    ])
+    def test_registration_invalid_email_format_shows_error(self, driver, invalid_email):
+        """При некорректном формате email показывается ошибка (ожидаем 'Некорректный email' или подсветку)"""
         driver.get(Urls.url_register)
 
         driver.find_element(*AuthRegistre.ar_name_field).send_keys('Ильин Виталий')
-        driver.find_element(*AuthRegistre.ar_email_field).send_keys(email_list)
+        driver.find_element(*AuthRegistre.ar_email_field).send_keys(invalid_email)
         driver.find_element(*AuthRegistre.ar_password_field).send_keys('123456')
-
         driver.find_element(*AuthRegistre.ar_register_button).click()
-        WebDriverWait(driver, 5).until(EC.presence_of_element_located(AuthRegistre.ar_error_message_2))
-        error_message = driver.find_element(*AuthRegistre.ar_error_message_2)
 
-        assert error_message.text == 'Такой пользователь уже существует'
+        error_elem = WebDriverWait(driver, 5).until(
+            EC.visibility_of_element_located(AuthRegistre.ar_error_message_invalid_email)
+        )
 
-    @pytest.mark.parametrize('password_list', ['1', '12345'])
-    def test_login_incorrect_password_less_six_symbols_show_error(self, driver, password_list):
-        """При вводе некорректного пароля, отображает ошибку 'Некорректный пароль'"""
+        assert error_elem.is_displayed(), "Не отобразилась ошибка при некорректном email"
+
+    @pytest.mark.parametrize('short_password', ['1', '12345'])
+    def test_registration_short_password_shows_error(self, driver, short_password):
+        """При пароле короче 6 символов отображается ошибка 'Некорректный пароль'"""
         driver.get(Urls.url_register)
 
         driver.find_element(*AuthRegistre.ar_name_field).send_keys('Ильин Виталий')
         driver.find_element(*AuthRegistre.ar_email_field).send_keys('test1@yan.ru')
-        driver.find_element(*AuthRegistre.ar_password_field).send_keys(password_list)
-
+        driver.find_element(*AuthRegistre.ar_password_field).send_keys(short_password)
         driver.find_element(*AuthRegistre.ar_register_button).click()
-        WebDriverWait(driver, 5).until(EC.presence_of_element_located(AuthRegistre.ar_error_message))
-        error_message = driver.find_element(*AuthRegistre.ar_error_message)
 
-        assert error_message.text == 'Некорректный пароль'
+        error_message = WebDriverWait(driver, 5).until(
+            EC.visibility_of_element_located(AuthRegistre.ar_error_message)
+        )
+
+        assert error_message.is_displayed(), "Не отобразилась ошибка при коротком пароле"
