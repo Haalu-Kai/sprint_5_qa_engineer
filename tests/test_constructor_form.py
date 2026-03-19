c164606b266c7043dcadb23d221856cbfa625382
Haalu-Kai <tinavie00@gmail.com>
2026-03-19 10:58:59 +0300

    Update test_constructor_form.py

--- tests/test_constructor_form.py
+++ tests/test_constructor_form.py
@@ -1,51 +1,44 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from web_locators.locators import *
 
 class TestStellarBurgersConstructorForm:
 
     def test_constructor_go_to_sauces_scroll_to_sauces(self, login):
-        """Проверка перехода на "Соусы" """
         driver = login
         driver.find_element(*MainPage.mn_constructor_button).click()
+        driver.find_element(*MainPage.mn_sauces_button).click()
 
-        sauces_tab = driver.find_element(*MainPage.mn_sauces_button)
-        sauces_tab.click()
+        def sauces_tab_active_and_header_visible():
+            tab = driver.find_element(*MainPage.tab_sauces_current)
+            header = driver.find_element(*MainPage.mn_h_sauces)
+            return tab.is_displayed() and "current" in tab.get_attribute("class") and header.is_displayed()
 
-        WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((MainPage.mn_sauces_button[0], MainPage.mn_sauces_button[1] + "[contains(@class, 'current')]"))
-        )
-
-  
-
-        h_sauce = driver.find_element(*MainPage.mn_h_sauces)
-        assert h_sauce.is_displayed(), "Заголовок 'Соусы' не отображается после переключения"
+        WebDriverWait(driver, 8).until(lambda d: sauces_tab_active_and_header_visible())
+        assert sauces_tab_active_and_header_visible(), "Таб 'Соусы' не стал активным или заголовок не виден"
 
     def test_constructor_go_to_filling_scroll_to_filling(self, login):
-        """Проверка перехода на "Начинки" """
         driver = login
         driver.find_element(*MainPage.mn_constructor_button).click()
+        driver.find_element(*MainPage.mn_filling_button).click()
 
-        filling_tab = driver.find_element(*MainPage.mn_filling_button)
-        filling_tab.click()
+        def filling_tab_active_and_header_visible():
+            tab = driver.find_element(*MainPage.tab_filling_current)
+            header = driver.find_element(*MainPage.mn_h_filling)
+            return tab.is_displayed() and "current" in tab.get_attribute("class") and header.is_displayed()
 
-        WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((MainPage.mn_filling_button[0], MainPage.mn_filling_button[1] + "[contains(@class, 'current')]"))
-        )
-
-        h_filling = driver.find_element(*MainPage.mn_h_filling)
-        assert h_filling.is_displayed()
+        WebDriverWait(driver, 8).until(lambda d: filling_tab_active_and_header_visible())
+        assert filling_tab_active_and_header_visible(), "Таб 'Начинки' не стал активным или заголовок не виден"
 
     def test_constructor_go_to_bun_scroll_to_bun(self, login):
-        """Проверка перехода на "Булки" """
         driver = login
         driver.find_element(*MainPage.mn_constructor_button).click()
+        driver.find_element(*MainPage.mn_bun_button).click()
 
+        def buns_tab_active_and_header_visible():
+            tab = driver.find_element(*MainPage.tab_buns_current)
+            header = driver.find_element(*MainPage.mn_h_bun)
+            return tab.is_displayed() and "current" in tab.get_attribute("class") and header.is_displayed()
 
-        bun_tab = driver.find_element(*MainPage.mn_bun_button)  
-        bun_tab.click()
-
-        WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((MainPage.mn_bun_button[0], MainPage.mn_bun_button[1] + "[contains(@class, 'current')]"))
-        )
-
-        h_bun = driver.find_element(*MainPage.mn_h_bun)  
-        assert h_bun.is_displayed()
+        WebDriverWait(driver, 8).until(lambda d: buns_tab_active_and_header_visible())
+        assert buns_tab_active_and_header_visible(), "Таб 'Булки' не стал активным или заголовок не виден"
